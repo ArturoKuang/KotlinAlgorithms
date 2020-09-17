44453618980594ae94cc64d0a8a4125bca46127d
axk4647 <arturokuang@gmail.com>
2020-09-16 22:23:22 -0400

    meeting ranges unit test

--- src/test/Interview_cake/MergeMeetingTimesTest.kt
+++ src/test/Interview_cake/MergeMeetingTimesTest.kt
@@ -1,5 +1,18 @@
 package Interview_cake
 
-class MergeMeetingTimesTest() {
+import interview_cake.Meeting
+import interview_cake.MergingMeetingTimes
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
+class MergeMeetingTimesTest() {
+    @Test
+    fun testMeetingRange() {
+        val meetings = listOf<Meeting>(Meeting(0, 1), Meeting(3, 5), Meeting(4, 8), Meeting(10, 12), Meeting(9, 10))
+        val expectedMeetings = listOf<Meeting>(Meeting(0, 1), Meeting(3, 8), Meeting(9, 12))
+
+        val mergingMeetingTimes = MergingMeetingTimes()
+
+        Assertions.assertEquals(mergingMeetingTimes.merge(meetings), expectedMeetings)
+    }
 }
