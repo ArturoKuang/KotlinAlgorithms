25e2f3242d333bf0472c8ba8edab9a196f8b593f
axk4647 <arturokuang@gmail.com>
2020-09-16 22:00:26 -0400

    LinkedList unit test

--- out/test/KotlinAlgorithms/src/test/data_structures/LinkedListTest.kt
+++ out/test/KotlinAlgorithms/src/test/data_structures/LinkedListTest.kt
@@ -1,64 +1,50 @@
 package data_structures
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.Test
 
 class LinkedListTest {
     @Test
-    fun emptyTest() {
-        //LinkedList<Int> list = LinkedList<Int>()
-        //Assertions.assertThrows(list.remove())
-    }
-
-    @Test
     fun addTest() {
-//        LinkedList<Int> list = LinkedList<Int>()
-//        for(i in 0..100) {
-//            list.add(i)
-//        }
-//
-//        for(i in 0..100) {
-//            Assertions.assertTrue(list.get(i) == i)
-//        }
+        var list = LinkedList<Int>()
+        for(i in 0..100) {
+            list.add(i)
+        }
+
+        for(i in 0..100) {
+            Assertions.assertTrue(list.get(i) == i)
+        }
     }
 
     @Test
     fun getTest() {
-//        LinkedList<Int> list = LinkedList<Int>()
-//        for(i in 0..100) {
-//             list.add(i)
-//        }
-//
-//        Assertions.assertTrue(list.get(50) == 50)
-//        Assertions.assertThrows(list.get(1000))
+        var list = LinkedList<Int>()
+        for(i in 0..100) {
+             list.add(i)
+        }
+
+        Assertions.assertTrue(list.get(50) == 50)
     }
 
     @Test
     fun removeTest() {
-//        LinkedList<Int> list = LinkedList<Int>()
-//        for(i in 0..100) {
-//            list.add(i)
-//        }
-//
-//        list.remove(10)
-//        list.remove()
-//
-//        for (i in 0..100) {
-//            if(i != 10 && i != 0) {
-//                Assertions.assertTrue(list.get(i) == i)
-//            }
-//        }
+        var list = LinkedList<Int>()
+        for(i in 0..100) {
+            list.add(i)
+        }
+
+        Assertions.assertTrue(list.remove(10) != null)
+        Assertions.assertTrue(list.remove(50) != null)
     }
 
     @Test
-    fun removeThrowTest() {
-        //LinkedList<Int> list = LinkedList<Int>()
-        //Assertions.assertThrows(list.remove())
-        var list = LinkedList<Int>()
-        list.add(1)
-        list.add(2)
-        list.add(3)
-        list.remove(1)
-        println(list.toString())
+    fun removeEmptyListTest() {
+        var  list = LinkedList<Int>()
+        try {
+            list.removeFirst()
+            list.remove(0)
+            Assertions.fail<String>("NoSuchElementException expected")
+        } catch (expectedException: NoSuchElementException) { }
     }
 }
