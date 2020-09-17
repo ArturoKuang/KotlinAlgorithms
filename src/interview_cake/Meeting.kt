ce5b7268b01ec48a2e56a90a791373f5d4b6bf0a
axk4647 <arturokuang@gmail.com>
2020-09-16 22:18:46 -0400

    meeting class interviewcake

--- src/interview_cake/Meeting.kt
+++ src/interview_cake/Meeting.kt
@@ -1,2 +1,4 @@
 package interview_cake
 
+class Meeting // number of 30 min blocks past 9:00 am
+(var startTime: Int, var endTime: Int)
--- src/interview_cake/MergingMeetingTimes.kt
+++ src/interview_cake/MergingMeetingTimes.kt
@@ -1,4 +1,7 @@
 package interview_cake
 
 class MergingMeetingTimes {
+    fun merge(meetingRanges: List<Meeting>): List<Meeting> {
+
+    }
 }
