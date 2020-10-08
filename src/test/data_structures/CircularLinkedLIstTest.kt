ac23598bddf7bbd2ab520613bf044dda08ed885e
axk4647 <arturokuang@gmail.com>
2020-10-08 15:57:44 -0400

    removed unused imports

--- src/test/data_structures/CircularLinkedLIstTest.kt
+++ src/test/data_structures/CircularLinkedLIstTest.kt
@@ -3,15 +3,13 @@ package data_structures
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertFails
 import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
 class CircularLinkedLIstTest {
 
     @Test
     fun addAtFrontTest() {
-        var list = CircularLinkedList()
+        val list = CircularLinkedList()
         val expected = listOf(10, 20)
         list.addAtFront(20)
         list.addAtFront(10)
@@ -21,7 +19,7 @@ class CircularLinkedLIstTest {
 
     @Test
     fun isEmptyTest() {
-        var list = CircularLinkedList()
+        val list = CircularLinkedList()
         assert(list.isEmpty())
 
         list.addAtFront(5)
@@ -31,7 +29,7 @@ class CircularLinkedLIstTest {
 
     @Test
     fun addAtEndTest(){
-        var list = CircularLinkedList()
+        val list = CircularLinkedList()
         val expected = listOf(10, 20)
         list.addAtEnd(10)
         list.addAtEnd(20)
@@ -41,7 +39,7 @@ class CircularLinkedLIstTest {
 
     @Test
     fun searchTest() {
-        var list = CircularLinkedList()
+        val list = CircularLinkedList()
         for(i in 0..100) {
             list.addAtEnd(i)
         }
@@ -53,7 +51,7 @@ class CircularLinkedLIstTest {
 
     @Test
     fun deleteNodeTest() {
-        var list = CircularLinkedList()
+        val list = CircularLinkedList()
         for(i in 0..100) {
             list.addAtEnd(i)
         }
@@ -64,7 +62,7 @@ class CircularLinkedLIstTest {
 
     @Test
     fun deleteEmptyTest() {
-        var list = CircularLinkedList()
+        val list = CircularLinkedList()
         throwSuchElementException {
             list.remove(10)
         }
