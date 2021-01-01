91bf0e06277583aec1805d6a9970d5d04eccba9b
axk4647 <arturokuang@gmail.com>
2020-12-31 23:53:52 -0500

    sorted stack

---
+++ src/CTCI/Chapter3/SortedStack.kt
@@ -0,0 +1,31 @@
+package CTCI.Chapter3
+
+import java.util.*
+import kotlin.random.Random.Default.nextInt
+
+fun sortStack(stack: ArrayDeque<Int>) {
+    val sortedStack = ArrayDeque<Int>()
+
+    while(stack.isNotEmpty()) {
+        val temp = stack.pop()
+        while (sortedStack.isNotEmpty() && sortedStack.peek() > temp) {
+            stack.push(sortedStack.pop())
+        }
+        sortedStack.push(temp)
+    }
+
+    while (sortedStack.isNotEmpty()) {
+        stack.push(sortedStack.pop())
+    }
+}
+
+fun main() {
+    val size = 10
+    val stack = ArrayDeque<Int>()
+    repeat(size) {
+        stack.push(Random().nextInt(9))
+    }
+    println("unsorted stack $stack")
+    sortStack(stack)
+    println("sorted stack $stack")
+}
